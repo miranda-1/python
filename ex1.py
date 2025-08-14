b5939a0cd804e1982de8cf2beca814c06806adc4
Miranda-Dev <joaovictor10_miranda@hotmail.com>
2025-08-14 19:11:56 -0400

    JAPA EXPRESS

---
+++ ex1.py
@@ -0,0 +1,10 @@
+print('Japa express\n')
+
+print('1-Cadastrar restaurante')
+print('2-Listar restaurante ')
+print('3-Ativar restaurante')
+print('4-Sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print('voce escolheu a opcao', opcao_escolhida)
+
