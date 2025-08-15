236abff4ceccb64636823cf182522771ba8b0b95
Miranda-Dev <joaovictor10_miranda@hotmail.com>
2025-08-15 14:22:08 -0400

    continuando minha primeira aplicação
    hj eu realizei mais atualizações no meu codigo

--- ex1.py
+++ ex1.py
@@ -1,10 +1,18 @@
-print('Japa express\n')
+print('🅙🅐🅟🅐 🅔🅧🅟🅡🅔🅢🅢\n')
 
 print('1-Cadastrar restaurante')
 print('2-Listar restaurante ')
 print('3-Ativar restaurante')
 print('4-Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print('voce escolheu a opcao', opcao_escolhida)
+opcao_escolhida = int(input('Escolha uma opção: '))
+# opcao_escolhida = int(opcao_escolhida)
 
+if opcao_escolhida == 1:
+    print('Cadastrar Restaurante')
+elif opcao_escolhida == 2:
+    print('Listar Restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar restaurante')
+else:
+    print('Encerrando o programa')
---
+++ ex2.py
@@ -0,0 +1,6 @@
+print('Python na Escola De Programaçao da alura\n')
+print('Meu nome eh João Victor Miranda Sacco e tenho 22 anos\n')
+print(' A \n L \n U \n R \n A \n')
+
+valor_pi = input ('Digite o valor de pi: ')
+print(f'o valor de pi eh:{valor_pi}')
