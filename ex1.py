ae593154bcb508b6d3169f4af14c93d928a706c3
Miranda-Dev <joaovictor10_miranda@hotmail.com>
2025-08-18 17:24:04 -0400

    aprendendo funçoes em python

--- ex1.py
+++ ex1.py
@@ -1,18 +1,34 @@
-print('🅙🅐🅟🅐 🅔🅧🅟🅡🅔🅢🅢\n')
+import os
+def exibir_o_nome(): 
+    print('🅙🅐🅟🅐 🅔🅧🅟🅡🅔🅢🅢\n')
 
-print('1-Cadastrar restaurante')
-print('2-Listar restaurante ')
-print('3-Ativar restaurante')
-print('4-Sair\n')
+def exibir_as_opcoes():
+    print('1-Cadastrar restaurante')
+    print('2-Listar restaurante ')
+    print('3-Ativar restaurante')
+    print('4-Sair\n')
 
-opcao_escolhida = int(input('Escolha uma opção: '))
-# opcao_escolhida = int(opcao_escolhida)
+def finalizando_app():
+    os.system('cls')
 
-if opcao_escolhida == 1:
-    print('Cadastrar Restaurante')
-elif opcao_escolhida == 2:
-    print('Listar Restaurantes')
-elif opcao_escolhida == 3:
-    print('Ativar restaurante')
-else:
-    print('Encerrando o programa')
+    print('Finalizando o app')
+
+def escolher_as_opcoes():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+
+    if opcao_escolhida == 1:
+        print('Cadastrar Restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar Restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    else:
+        finalizando_app()
+
+def main():
+    exibir_o_nome()
+    exibir_as_opcoes()
+    escolher_as_opcoes()
+
+if __name__ == '__main__':
+    main()
